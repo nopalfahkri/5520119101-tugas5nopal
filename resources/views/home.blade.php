284200ca7d1f98101c7f89021f8d843309eba40c
nopalfahkri <fahkrinopal@gmail.com>
2021-05-01 23:17:22 +0700

    tugas besar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,16 +6,19 @@
     <h1>Dashboard</h1>
 @stop
 
+
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+    <p>login as user</p>
+@stop
+
 @section('footer')
 <div class="float-right d-none d-sm-block">
     <b>Version</b> 1.0.0
 </div>
 <strong>CopyRight &copy; {{date('Y')}}
     <a href="#" target="_blank">naufal fahkri</a>.</strong>All Right reserved
-
-@section('content')
-    <p>Welcome to this beautiful admin panel.</p>
-    <p>login as user</p>
 @stop
 
 @section('css')
